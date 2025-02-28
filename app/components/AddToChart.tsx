58c94f7f66491711de15df99523c1994c916ae32
Rui-Yan03 <yan70834@gmail.com>
2025-02-28 17:46:05 +0800

    first 30 mintues

---
+++ app/components/AddToChart.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const AddToChart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log('add to chart')}>Add to chart</button>
+    </div>
+  )
+}
+
+export default AddToChart
