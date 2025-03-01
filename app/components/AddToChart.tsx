ac365b1bf639209f6d5adc1000a80254458f7980
Rui-Yan03 <yan70834@gmail.com>
2025-03-01 11:59:07 +0800

    frist free hour done

--- app/components/AddToChart.tsx
+++ app/components/AddToChart.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const AddToChart = () => {
   return (
-    <div>
-      <button onClick={() => console.log('add to chart')}>Add to chart</button>
+    <div className='p-4 my-4 bg-sky-600 text-white text-xl hover:bg-sky-700'>
+      <button className='btn btn-primary' onClick={() => console.log('add to chart')}>Add to chart</button>
     </div>
   )
 }
